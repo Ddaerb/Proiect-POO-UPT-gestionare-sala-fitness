af4ff2089213e611911b6da14a5676a5d69ded3d
roxiiii <roxy2004.tm@gmail.com>
2024-12-29 16:04:52 +0200

    Adaugare metoda pentru verificarea cnp-ului unui abonat in clasa Validare

--- WinFormsApp1/Validare.cs
+++ WinFormsApp1/Validare.cs
@@ -1,4 +1,5 @@
 ﻿namespace WinFormsApp1;
+using System.Linq; //folosim metoda All din LINQ impreuna cu functia char.IsDigit pentru a verifica daca un string contine doar cifre
 
 public class Validare
 {
@@ -60,5 +61,11 @@ public class Validare
             }
         }
         return true;
+    }
+
+    //metoda pentru verificarea cnp-ului unui abonat
+    public static bool VerificareCNP(string cnp)
+    {
+        return cnp.Length == 13 && cnp.All(char.IsDigit);
     }
 }
