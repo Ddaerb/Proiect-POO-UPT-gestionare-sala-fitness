5ab8853710b7025c912ea8cedbadd01563136d0c
roxiiii <roxy2004.tm@gmail.com>
2024-12-30 18:03:39 +0200

    Modificat clasa Validare => adaugat metoda ValidareData

--- WinFormsApp1/Validare.cs
+++ WinFormsApp1/Validare.cs
@@ -1,13 +1,12 @@
 ﻿namespace WinFormsApp1;
 using System.Linq; //folosim metoda All din LINQ impreuna cu functia char.IsDigit pentru a verifica daca un string contine doar cifre
-
 public class Validare
 {
     public static string ValideazaNume(string input)
     {
         while (string.IsNullOrWhiteSpace(input) || !IsAlphabetic(input))
         {
-            Console.WriteLine("Numele trebuie sa contina doar caractere alfabetice. Introdu din nou:");
+            Console.WriteLine("Numele trebuie să conțină doar caractere alfabetice. Introdu din nou:");
             input = Console.ReadLine();
         }
         return input;
@@ -18,7 +17,7 @@ public class Validare
         int rezultat;
         while (!int.TryParse(input, out rezultat) || rezultat < min || rezultat > max)
         {
-            Console.WriteLine($"Introduceti un numar intre {min} si {max}:");
+            Console.WriteLine($"Introduceți un număr între {min} și {max}:");
             input = Console.ReadLine();
         }
         return rezultat;
@@ -29,7 +28,7 @@ public class Validare
         TimeSpan ora;
         while (!TimeSpan.TryParse(input, out ora))
         {
-            Console.WriteLine("Ora nu este intr-un format valid (HH:MM). Introdu din nou:");
+            Console.WriteLine("Ora nu este într-un format valid (HH:MM). Introdu din nou:");
             input = Console.ReadLine();
         }
         return ora;
@@ -44,7 +43,7 @@ public class Validare
 
         while (oraInceput >= oraSfarsit)
         {
-            Console.WriteLine("Ora de inceput trebuie sa fie mai mica decat ora de sfarsit.");
+            Console.WriteLine("Ora de început trebuie să fie mai mică decât ora de sfârșit.");
             oraInceput = ValideazaOra(Console.ReadLine());
             oraSfarsit = ValideazaOra(Console.ReadLine());
         }
@@ -63,9 +62,34 @@ public class Validare
         return true;
     }
 
-    //metoda pentru verificarea cnp-ului unui abonat
+    //Metoda pentru verificarea cnp-ului unui abonat
     public static bool VerificareCNP(string cnp)
     {
         return cnp.Length == 13 && cnp.All(char.IsDigit);
     }
+
+    //Metoda pentru validarea unei date (sa nu fie mai mica ca data curenta si sa fie o data valida)
+    public static bool ValidareData(DateTime data)
+    {
+        if (data.Day < 1 || data.Day > DateTime.DaysInMonth(data.Year, data.Month))
+        {
+            Console.WriteLine("Ziua introdusa nu este corecta");
+            return false;
+        }
+
+        if (data.Month < 1 || data.Month > 12)
+        {
+            Console.WriteLine("Luna introdusa nu este corecta");
+            return false;
+        }
+
+        if (data < DateTime.Now)
+        {
+            Console.WriteLine("Data nu trebuie sa fie in trecut");
+            return false;
+        }
+        return true;
+    }
+
+
 }
