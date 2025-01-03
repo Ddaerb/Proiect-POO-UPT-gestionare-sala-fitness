6a865a206e9cfc4d8e6b4066f1ebd878ebe90602
Raz-Seb <razvantrufin2003@gmail.com>
2025-01-03 19:44:59 +0200

    Modificare validari

--- WinFormsApp1/Validare.cs
+++ WinFormsApp1/Validare.cs
@@ -1,40 +1,44 @@
-﻿namespace WinFormsApp1;
+﻿using System.Data;
+
+namespace WinFormsApp1;
 using System.Linq; //folosim metoda All din LINQ impreuna cu functia char.IsDigit pentru a verifica daca un string contine doar cifre
+
 public class Validare
 {
-    public static string ValideazaNume(string input)
+    public string ValideazaNume(string input)
     {
         while (string.IsNullOrWhiteSpace(input) || !IsAlphabetic(input))
         {
-            Console.WriteLine("Numele trebuie să conțină doar caractere alfabetice. Introdu din nou:");
+            Console.WriteLine("Numele trebuie sa contina doar caractere alfabetice. Introdu din nou:");
             input = Console.ReadLine();
         }
         return input;
     }
 
-    public static int ValideazaNumarIntreg(string input, int min, int max)
+    public int ValideazaNrClienti(string input)
     {
         int rezultat;
-        while (!int.TryParse(input, out rezultat) || rezultat < min || rezultat > max)
+        while (!int.TryParse(input, out rezultat) || rezultat < 1 || rezultat > 10)
         {
-            Console.WriteLine($"Introduceți un număr între {min} și {max}:");
+            Console.WriteLine($"Introduceti un numar intre 1 si 10:");
             input = Console.ReadLine();
         }
         return rezultat;
     }
 
-    public static TimeSpan ValideazaOra(string input)
+    public TimeSpan ValideazaOra(string input)
     {
         TimeSpan ora;
         while (!TimeSpan.TryParse(input, out ora))
         {
-            Console.WriteLine("Ora nu este într-un format valid (HH:MM). Introdu din nou:");
+            Console.WriteLine("Ora nu este intr-un format valid (HH:MM). Introdu din nou:");
             input = Console.ReadLine();
         }
         return ora;
     }
 
-    public static (TimeSpan, TimeSpan) ValideazaInterval(string oraInceputInput, string oraSfarsitInput)
+    // trebuie modificata
+    public (TimeSpan, TimeSpan) ValideazaInterval(string oraInceputInput, string oraSfarsitInput)
     {
         TimeSpan oraInceput, oraSfarsit;
 
@@ -43,14 +47,14 @@ public class Validare
 
         while (oraInceput >= oraSfarsit)
         {
-            Console.WriteLine("Ora de început trebuie să fie mai mică decât ora de sfârșit.");
+            Console.WriteLine("Ora de inceput trebuie sa fie mai mica decat ora de sfarsit.");
             oraInceput = ValideazaOra(Console.ReadLine());
             oraSfarsit = ValideazaOra(Console.ReadLine());
         }
         return (oraInceput, oraSfarsit);
     }
 
-    private static bool IsAlphabetic(string input)
+    private bool IsAlphabetic(string input)
     {
         foreach (char c in input)
         {
@@ -62,34 +66,57 @@ public class Validare
         return true;
     }
 
-    //Metoda pentru verificarea cnp-ului unui abonat
-    public static bool VerificareCNP(string cnp)
+    //metoda pentru verificarea cnp-ului unui abonat
+    public string VerificareCNP(string cnp)
     {
-        return cnp.Length == 13 && cnp.All(char.IsDigit);
+        while (!(cnp.Length == 13) || !(cnp.All(char.IsDigit)))
+        {
+            Console.WriteLine("CNP-ul nu este intr-un format valid.");
+            cnp = Console.ReadLine();
+        }
+        return cnp;
     }
 
     //Metoda pentru validarea unei date (sa nu fie mai mica ca data curenta si sa fie o data valida)
-    public static bool ValidareData(DateTime data)
+    public DateTime ValidareData(string dataInput)
     {
+        DateTime data;
+
+        if (!DateTime.TryParse(dataInput, out data))
+        {
+            Console.WriteLine("Formatul introdus nu este corect. Introduceti o data valida (ex. 'YYYY/MM/DD').");
+            return ValidareData(Console.ReadLine());
+        }
+
         if (data.Day < 1 || data.Day > DateTime.DaysInMonth(data.Year, data.Month))
         {
-            Console.WriteLine("Ziua introdusa nu este corecta");
-            return false;
+            Console.WriteLine("Ziua introdusa nu este corecta.");
+            return ValidareData(Console.ReadLine());
         }
 
         if (data.Month < 1 || data.Month > 12)
         {
-            Console.WriteLine("Luna introdusa nu este corecta");
-            return false;
+            Console.WriteLine("Luna introdusa nu este corecta.");
+            return ValidareData(Console.ReadLine());
         }
 
         if (data < DateTime.Now)
         {
-            Console.WriteLine("Data nu trebuie sa fie in trecut");
-            return false;
+            Console.WriteLine("Data nu trebuie sa fie in trecut.");
+            return ValidareData(Console.ReadLine());
         }
-        return true;
+
+        return data;
     }
 
+    public string ValideazaNumeComplet(string input)
+    {
+        while (!IsAlphabetic(input))
+        {
+            Console.WriteLine("Numele complet trebuie sa contina doar caractere alfabetice. Introdu din nou:");
+            input = Console.ReadLine();
+        }
+        return input;
+    }
 
 }
